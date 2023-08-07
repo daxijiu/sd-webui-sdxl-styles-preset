4f6af18fe7d94ffa0d7dfb94437672389f87c3d6
daxijiu <127850313+daxijiu@users.noreply.github.com>
2023-08-07 17:54:18 +0800

    Update install.py

--- install.py
+++ install.py
@@ -6,8 +6,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 #如果是初次安装，创建installed文件作为标记，并修改采样器样式为下拉菜单，如果已经有installed文件则跳过。
 if not os.path.exists(os.path.join(current_dir, 'installed')):
-    with open(os.path.join(current_dir, 'installed'), 'w') as f1:
-        f1.write('')
+#    with open(os.path.join(current_dir, 'installed'), 'w') as f1:
+#        f1.write('')
 
 # 获取 config.json 文件的绝对路径
     json_file_path = os.path.join(current_dir, '..', '..', 'config.json')
